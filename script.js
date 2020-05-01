1e26b72ad7ab2dc209a001a81cee00b2c9c0f6ef
Jamie Evans <jamieevans96@googlemail.com>
2020-05-02 00:51:53 +0100

    half way

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 const content = document.querySelector("#content");
 
-for (i = 1; i < 257; i++) {
-    const div = document.createElement('div');
-    div.setAttribute('id', 'div');
+    for (i = 1; i < 257; i++) {
+        const div = document.createElement('div');
 
-    div.addEventListener('mouseover', function (e) {
-        e.target.style.background = 'black';
-    });
+        div.addEventListener('mouseover', function (e) {
+            e.target.style.background = 'black';
+        });
 
-    content.appendChild(div);
+        content.appendChild(div);
+    }
+
+function clearFunc() {
+
+    var y = prompt("number");
+
+    document.getElementById("content").style.gridTemplateRows = "repeat(" + y + ", " + (960/y) + "px)";
+    document.getElementById("content").style.gridTemplateColumns = "repeat(" + y + ", " + (960/y) + "px)";
+
+    let x = (y * y) + 1;
+
+    for (i = 1; i < x; i++) {
+        const div = document.createElement('div');
+
+        div.addEventListener('mouseover', function (e) {
+            e.target.style.background = 'black';
+        });
+
+        content.appendChild(div);
+    }
 }
